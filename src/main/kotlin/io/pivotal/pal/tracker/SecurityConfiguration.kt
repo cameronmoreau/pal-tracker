05943d6e498573dddf2a183f6881e1195b238e8b
Peter Chen <pchen@pivotal.io>
2018-07-24 16:35:50 -0700

    HTTP Basic authentication and SSL with Spring Security
    Signed-off-by: Peter Chen <pchen@pivotal.io>

---
+++ src/main/kotlin/io/pivotal/pal/tracker/SecurityConfiguration.kt
@@ -0,0 +1,27 @@
+package io.pivotal.pal.tracker
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class SecurityConfiguration(
+        @Value("\${HTTPS_DISABLED:NOT SET}") var httpsDisabled: Boolean) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+
+        if (!httpsDisabled) {
+            http.requiresChannel().anyRequest().requiresSecure();
+        }
+
+        http.authorizeRequests().antMatchers("/**").hasRole("USER").and().httpBasic()
+        http.csrf().disable()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+    }
+
+}
